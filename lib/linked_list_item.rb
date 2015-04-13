580322348f80d45cfa939103a88dbd6be3697fde
brayhoward <brandonrhoward@gmail.com>
2015-04-13 16:01:33 -0500

    working on LinkedListItem method

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,17 @@
 class LinkedListItem
-  include Comparable
+	include Comparable
+	attr_reader :payload
+
+	def initialize(data)
+		@payload = data
+	end
+	
 end
+
+#letting the test guid me took me to here
+#########################################
+# def next_item=(data)
+# 	def next_item
+
+# 	end
+# end
